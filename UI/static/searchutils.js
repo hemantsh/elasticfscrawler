e1977024f9a1be81cb40b764c0c9070f4057e968
Hemant <sh.hemant@gmail.com>
2023-07-06 01:03:27 -0700

    fixed long label issue

--- UI/static/searchutils.js
+++ UI/static/searchutils.js
@@ -1,4 +1,73 @@
 
+tooltipCallback = function(context) {
+    // Tooltip Element
+    let tooltipEl = document.getElementById('chartjs-tooltip');
+    // Create element on first render
+    if (!tooltipEl) {
+        tooltipEl = document.createElement('div');
+        tooltipEl.id = 'chartjs-tooltip';
+        tooltipEl.innerHTML = '<table></table>';
+        document.body.appendChild(tooltipEl);
+    }
+
+    // Hide if no tooltip
+    const tooltipModel = context.tooltip;
+    if (tooltipModel.opacity === 0) {
+        tooltipEl.style.opacity = 0;
+        return;
+    }
+
+    // Set caret Position
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (tooltipModel.yAlign) {
+        tooltipEl.classList.add(tooltipModel.yAlign);
+    } else {
+        tooltipEl.classList.add('no-transform');
+    }
+
+    function getBody(bodyItem) {
+        return bodyItem.lines;
+    }
+
+    // Set Text
+    if (tooltipModel.body) {
+        const titleLines = tooltipModel.title || [];
+        const bodyLines = tooltipModel.body.map(getBody);
+
+        let innerHtml = '<thead>';
+
+        titleLines.forEach(function(title) {
+            innerHtml += '<tr><th>' + title + '</th></tr>';
+        });
+        innerHtml += '</thead><tbody>';
+
+        bodyLines.forEach(function(body, i) {
+            const colors = tooltipModel.labelColors[i];
+            let style = 'background:' + colors.backgroundColor;
+            style += '; border-color:' + colors.borderColor;
+            style += '; border-width: 2px';
+            const span = '<span style="' + style + '">' + body + '</span>';
+            innerHtml += '<tr><td>' + span + '</td></tr>';
+        });
+        innerHtml += '</tbody>';
+
+        let tableRoot = tooltipEl.querySelector('table');
+        tableRoot.innerHTML = innerHtml;
+    }
+
+    const position = context.chart.canvas.getBoundingClientRect();
+    const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
+
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+    tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+    tooltipEl.style.font = bodyFont.string;
+    tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
+    tooltipEl.style.pointerEvents = 'none';
+    
+}
 
 function populateSummary() {
     var data = document.getElementsByName("goodSummary");
@@ -63,8 +132,8 @@ function getChartDatasets() {
             }
         });
     
-    Object.keys(casefiles).forEach(function (caseNum) {
-        datasets[caseNum] = $.map(casefiles[caseNum], function (path) {
+        Object.keys(casefiles).forEach(function (caseNum) {
+            datasets[caseNum] = $.map(casefiles[caseNum], function (path) {
 
             return {
                 name: path,
@@ -94,6 +163,7 @@ function drawChart(index, caseid, dataset) {
             ]
         },
         options: {
+            
             plugins: {
                 legend: {
                     display: false
@@ -107,15 +177,12 @@ function drawChart(index, caseid, dataset) {
                     enabled: false
                 },
                 tooltip: {
-                    callbacks: {
-                        title: function (tooltipItems) {
-                          return tooltipItems[0].parsed.x;
-                        },
-                        label: function(data) {  return "" }
-                    }
+                    enabled: false,
+                    external: tooltipCallback
                 }
             }
         }
     });
+    
     
 }
