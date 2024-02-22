bbc2c59c3117715829cfb6462acff35d96c47140
kanikabiz4group <kanika.singhal@biz4group.com>
2024-02-22 19:15:26 +0530

    Profile and Login, Logout Functionality
    Profile and Login - Logout are working perfectly

--- UI/app.py
+++ UI/app.py
@@ -26,13 +26,10 @@ password = os.getenv('ELASTIC_PASSWORD')
 host = os.getenv('ELASTIC_HOST')
 indexName = os.getenv('INDEX_NAME')
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:RKKanika_SinghalBiz4_@127.0.0.1/elasticsearch21' 
 es = Elasticsearch(host,basic_auth=(user,password),verify_certs=False)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-# app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 
 # Define User model
@@ -50,6 +47,7 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
+
 # Define Search model
 class Search(db.Model):  
     id = db.Column(db.Integer, primary_key=True)
@@ -57,46 +55,76 @@ class Search(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     search_query = db.Column(db.String(255))
 
+
 # Initialize login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
+# @login_manager.user_loader
+# def load_user(email):
+#     return User.query.filter_by(email=email).first()
+
+
 @login_manager.user_loader
-def load_user(user_id):
+def load_user(user_id): 
     return User.query.get(int(user_id))
+
 
 @app.route('/')
 def index():
-    return redirect(url_for('register'))
+    return redirect(url_for('login'))
+
+
+# @app.route('/home/')
+# @login_required
+# def home():
+#     return render_template('results.html', displayname=current_user.displayname)
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
         displayname = request.form['displayname']
         password = request.form['password']
-        
+        countries = request.form['countries']
+
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email address already exists.')
             return redirect(url_for('register'))
+
+        if current_user.is_authenticated:
+            # User is logged in, update profile
+            current_user.email = email
+            current_user.displayname = displayname
+            current_user.set_password(password)
+            current_user.countries = countries
+            db.session.commit()
+            flash('Your profile has been updated successfully!', 'success')
+            return redirect(url_for('login'))  # Redirect to login after profile update
         
-        new_user = User(email=email, displayname=displayname)
-        new_user.set_password(password)  
+
+        # User is not logged in, create new user
+        new_user = User(email=email, displayname=displayname, countries=countries)
+        new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
 
-        if current_user.is_authenticated:
-            # User is logged in
-            user_email = current_user.email
-            print(f"User with email '{user_email}' is logged in.")
-        else:
-            # User is not logged in
-            print("No user is logged in.")
-
-        flash(f'User registered successfully with email: {email}')
+        flash('User registered successfully with email: {}'.format(email), 'success')
         return redirect(url_for('login'))
-    
-    return render_template('register.html')
+
+    # GET request
+    if current_user.is_authenticated:
+        # User is logged in, pre-fill form fields
+        email = current_user.email
+        displayname = current_user.displayname
+        countries = current_user.countries
+        return render_template('register.html', title='My Profile', email=email, displayname=displayname, countries=countries, save_button=True)
+    else:
+        # User is not logged in, render registration form
+        return render_template('register.html', title='Registration')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -123,7 +151,9 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/search/results', methods=['POST'])
+@login_required
 def search():
     if request.method == 'POST':
         search_query = request.form['input']
@@ -144,12 +174,16 @@ def search():
 
     return render_template('results.html', res=res, input=search_query, user_id=user_id)  
 
+
 @app.route('/home/')
+@login_required
 def home():
-    return render_template('results.html')
+    return render_template('results.html', displayname=current_user.displayname)
     
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create all database tables
     app.secret_key = 'supersecretkey'  # Secret key for flashing messages
     app.run('127.0.0.1', debug=True)
+
